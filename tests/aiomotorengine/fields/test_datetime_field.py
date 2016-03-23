8e4ebc880626877b958dd40022ab595d091949df
ilex <ilexcorp@ukr.net>
2016-03-24 00:43:08 +0200

    Add asyncio tests for auto_now_on_insert and auto_now_on_update for DateTimeField in embedded documents

--- tests/aiomotorengine/fields/test_datetime_field.py
+++ tests/aiomotorengine/fields/test_datetime_field.py
@@ -2,7 +2,9 @@ import asyncio
 from datetime import datetime
 from preggy import expect
 
-from motorengine.aiomotorengine import Document, DateTimeField
+from motorengine.aiomotorengine import (
+    Document, DateTimeField, EmbeddedDocumentField
+)
 from tests.aiomotorengine import AsyncTestCase, async_test
 
 
@@ -72,3 +74,72 @@ class TestDateTimeField(AsyncTestCase):
         expect(model.created).to_be_lesser_or_equal_to(now_after)
         created = model.created
         expect(created).to_equal(model.created)
+
+    @async_test
+    @asyncio.coroutine
+    def test_embedded_document_with_auto_insert_datetime_field(self):
+
+        class Model(Document):
+            created = DateTimeField(
+                auto_now_on_insert=True, auto_now_on_update=False
+            )
+
+        class Doc(Document):
+            embedded = EmbeddedDocumentField(
+                embedded_document_type=Model, required=True
+            )
+
+        yield from self.drop_coll_async(Doc.__collection__)
+        yield from self.drop_coll_async(Model.__collection__)
+
+        doc = Doc(embedded=Model())
+
+        expect(doc.embedded.created).to_be_null()
+
+        yield from doc.save()
+
+        expect(doc.embedded.created).not_to_be_null()
+        expect(isinstance(doc.embedded.created, datetime)).to_be_true()
+
+    @async_test
+    @asyncio.coroutine
+    def test_embedded_document_with_auto_update_datetime_field(self):
+
+        class Model(Document):
+            created = DateTimeField(
+                auto_now_on_insert=True, auto_now_on_update=True
+            )
+
+        class Doc(Document):
+            embedded = EmbeddedDocumentField(
+                embedded_document_type=Model, required=False
+            )
+
+        yield from self.drop_coll_async(Doc.__collection__)
+        yield from self.drop_coll_async(Model.__collection__)
+
+        doc = yield from Doc.objects.create()
+
+        expect(doc.embedded).to_be_null()
+
+        doc = Doc(embedded=Model())
+
+        expect(doc.embedded.created).to_be_null()
+
+        now_before = datetime.now()
+        yield from doc.save()
+        now_after = datetime.now()
+
+        expect(doc.embedded.created).not_to_be_null()
+        expect(isinstance(doc.embedded.created, datetime)).to_be_true()
+        expect(doc.embedded.created).to_be_greater_or_equal_to(now_before)
+        expect(doc.embedded.created).to_be_lesser_or_equal_to(now_after)
+
+        created = doc.embedded.created
+
+        yield from doc.save()
+
+        expect(doc.embedded.created).not_to_be_null()
+        expect(isinstance(doc.embedded.created, datetime)).to_be_true()
+        expect(doc.embedded.created).to_be_greater_than(created)
+        expect(doc.embedded.created).to_be_lesser_or_equal_to(datetime.now())
