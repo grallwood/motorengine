030e5a1a733057b0f5f29fdc46b8ed20c277cd11
Björn Friedrichs <bjoern@friedrichs1.de>
2018-03-08 15:15:53 +0100

    Remove replicaSet test from aiomotorengine specific tests which checks for keyword validity and isn't part of motorengine itself

--- tests/aiomotorengine/test_connect.py
+++ tests/aiomotorengine/test_connect.py
@@ -60,12 +60,3 @@ class TestConnect(AsyncTestCase):
         res = yield from db.ping()
         ping_result = res['ok']
         expect(ping_result).to_equal(1.0)
-
-    def test_connect_to_replica_set_with_invalid_replicaset_name(self):
-        try:
-            connect('test', host="localhost:27017,localhost:27018", replicaSet=0, port=27017, io_loop=self.io_loop)
-        except ConnectionError:
-            exc_info = sys.exc_info()[1]
-            expect(str(exc_info)).to_include('the replicaSet keyword parameter is required')
-        else:
-            assert False, "Should not have gotten this far"
